7544dcf54867e0300ee2d2c8bb543d271e1f99cf
narramuni <nmspavan@gmail.com>
2023-10-17 11:25:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedListPro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+       
+        
+
+        public static void runApp()
+        {   
+
+            SinglyLinkedList list = new SinglyLinkedList();
+            list.CreateSinglyLinkedList();
+            Console.ReadLine();
+
+
+
+           
+        }
+
+    }
+}
